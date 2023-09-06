ec1a5860910f78a7586727800665dd5acd221d38
Akash Sahu <akashsahu793@gmail.com>
2023-09-06 18:18:43 +0530

    added gh-pages and published with prb"

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ const App = () => {
         <Navbar />
         <Routes>
             <Route path="/" exact element= {<Feed />} />
-              <Route path="/video/:id"  element={<VideoDetail />} />
-              <Route path="/channel/:id"  element={<ChannelDetail />} />
-              <Route path="/search/:searchTerm"  element={<SearchFeed />}/>
+            <Route path="/video/:id"  element={<VideoDetail />} />
+            <Route path="/channel/:id"  element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm"  element={<SearchFeed />}/>
 
         </Routes>
       </Box>
